3fbac28984f9de92f96c8132ea16940440a5ffc7
Desmond Mullen <dsm@busylittlestudios.com>
2019-06-03 15:03:34 -0400

    basic function

--- live_w_locator.js
+++ live_w_locator.js
@@ -107,7 +107,7 @@ setTimeout(function () {
     App.init();
     setTimeout(function () {
         App.stopIfNotDisplaying();
-    }, 250);
+    }, 500);
 
     Quagga.onDetected(function (result) {
         var code = result.codeResult.code;
